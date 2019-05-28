94928d27fad2e6c930a6e59469926737ea8db3b9
施华军 <shihuajunze@163.com>
2019-05-28 16:06:33 +0800

    add

--- metadata.js
+++ metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "helpers": {
-    "if_or": function (v1, v2, options) {
+  helpers: {
+    if_or: function (v1, v2, options) {
       if (v1 || v2) {
         return options.fn(this);
       }
@@ -8,30 +8,28 @@ module.exports = {
       return options.inverse(this);
     }
   },
-  "prompts": {
-    "name": {
-      "type": "string",
-      "required": true,
-      "message": "Project name"
+  prompts: {
+    name: {
+      type: "string",
+      required: true,
+      message: "Project name"
     },
-    "description": {
-      "type": "string",
-      "required": false,
-      "message": "Project description",
+    description: {
+      type: "string",
+      required: false,
+      message: "Project description",
     },
-    "author": {
-      "type": "string",
-      "message": "Author"
+    author: {
+      type: "string",
+      message: "Author"
     },
   },
-  "filters": {
-    ".eslintrc.js": "lint",
-    ".eslintignore": "lint",
-    "config/test.env.js": "unit || e2e",
-    "test/unit/**/*": "unit",
-    "build/webpack.test.conf.js": "unit",
-    "test/e2e/**/*": "e2e",
-    "src/router/**/*": "router"
+  eslint: {
+    type: "confirm",
+    message: "Use ESLint to lint your code?"
   },
-  "completeMessage": "To get started:\n\n  {{^inplace}}cd {{destDirName}}\n  {{/inplace}}npm install\n  npm start\n\n"
+  filters: {
+    ".eslintrc.js": "eslint",
+  },
+  completeMessage: "To get started:\n\n  {{^inplace}}cd {{destDirName}}\n  {{/inplace}}npm install\n  npm start\n\n"
 }
